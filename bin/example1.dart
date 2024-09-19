07c7cb6e58dc640db80b12c32e7712f7250c32fc
Javoh29 <javohergashev97@gmail.com>
2024-09-19 16:25:02 +0500

    [MB-1001] changed text

--- bin/example1.dart
+++ bin/example1.dart
@@ -1,5 +1,5 @@
 import 'package:example1/example1.dart' as example1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${example1.calculate()}!');
+  print('World: ${example1.calculate()}!');
 }
